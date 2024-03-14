22b4d2194f8909b1be3ed16a473c6af2da094f12
Koles <foockpw@gmail.com>
2024-03-14 09:45:22 +0100

    addconnection12

--- createaforeconect.js
+++ createaforeconect.js
@@ -7,7 +7,7 @@ function checkRefresh() {
     const currentTimestamp = Date.now();
 
     if (currentTimestamp - lastTimestamp > 10000 && isRunning) {
-        // Page refreshed or reloaded
+       // Page refreshed or reloaded
         lastTimestamp = currentTimestamp;
 
         if (clickLink) {
@@ -16,13 +16,11 @@ function checkRefresh() {
                 linkElement.click();
                 clickLink = false;
 
-                // Wait for 2.5 seconds before clicking the next element
                 setTimeout(() => {
                     const ghButtonElement = document.querySelector('.gh_button.gh_code');
                     if (ghButtonElement) {
                         ghButtonElement.click();
 
-                        // Wait for another 2.5 seconds before clicking the last element
                         setTimeout(() => {
                             const codeButtonElement = document.querySelector('.code_button.code_code');
                             if (codeButtonElement) {
@@ -40,13 +38,11 @@ function toggleScript() {
     isRunning = !isRunning;
 
     if (isRunning) {
-        // Start the script
         localStorage.setItem('isRunning', 'true');
         refreshInterval = setInterval(() => {
             checkRefresh();
         }, 1000);
     } else {
-        // Stop the script
         localStorage.setItem('isRunning', 'false');
         clearInterval(refreshInterval);
     }
@@ -72,31 +68,26 @@ function checkIfRefreshed() {
 }
 
 window.addEventListener('beforeunload', () => {
-    lastTimestamp = Date.now(); // Reset timestamp on page unload
+    lastTimestamp = Date.now(); 
 });
 
-// Check localStorage for the previous state
 const storedState = localStorage.getItem('isRunning');
 if (storedState === 'true') {
     toggleScript();
 }
-
-// Create a button to control the script
 function createControlButton() {
     const controlButton = document.createElement('button');
     controlButton.id = 'toggleScriptButton';
     controlButton.textContent = isRunning ? 'Stop' : 'Start';
-    controlButton.className = 'btn_small_gold'; // Added class 'btn_small_gold'
-    controlButton.style.position = 'fixed';
-    controlButton.style.top = '10px';
+    controlButton.className = 'btn_small_gold';
+    controlButton.style.position = 'absolute';
+    controlButton.style.top = '30px';
     controlButton.style.right = '10px';
     controlButton.addEventListener('click', () => {
         toggleScript();
-        checkIfRefreshed(); // Check immediately after clicking the button
+        checkIfRefreshed(); 
     });
     document.body.appendChild(controlButton);
 }
-
-// Delay creating the button to ensure it's added after the page has loaded
 setTimeout(createControlButton, 2000);
 
